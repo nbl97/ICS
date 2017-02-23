d7a48a4c0e9dc11972d7c04b81caf7ef2d5abd2a
tracer-ics2015 <tracer@njuics.org>
2017-02-24 04:30:32 +0800

    >  compile NEMU 161530217 nibolin Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  04:30:32 up  3:53,  1 user,  load average: 0.08, 0.02, 0.01 a83ddf6df4a4ef1e639899d6521d86b2b70f08f9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -58,7 +58,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-	{ "si","Single Step", cmd_si},
+	{ "si","Single Step", cmd_si },
 
 	/* TODO: Add more commands */
 

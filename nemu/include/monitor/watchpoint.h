d60753b6d3cdb12eb951cbc39e4dcfa0d39d613b
nbl97 <1035833260@qq.com>
2017-03-17 22:11:28 +0800

    修改了watchpoint.h
    完成了结构体和函数的声明。

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -6,10 +6,17 @@
 typedef struct watchpoint {
 	int NO;
 	struct watchpoint *next;
-
+	uint32_t oldValue,newValue;
+	char expr[100];
 	/* TODO: Add more members if necessary */
-
-
 } WP;
+
+void init_wp_list();
+WP* new_wp();
+void free_wp(WP *wp);
+int check_watchpoints();
+void set_watchpoint(char *args);
+void delete_watchpoint(int k);
+void list_watchpoint();
 
 #endif

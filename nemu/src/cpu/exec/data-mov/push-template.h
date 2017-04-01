59e1e5e26ed0e0ef2cea41e5d59e97ba5c761ff8
nbl97 <1035833260@qq.com>
2017-04-01 22:22:33 +0800

    完成push的三个函数

---
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -0,0 +1,13 @@
+#include "cpu/exec/template-start.h"
+#define instr push
+static void do_execute() {
+	cpu.esp = cpu.esp - 4;
+	swaddr_write(cpu.esp,4,op_src->val);
+	print_asm_template1();
+}
+
+make_instr_helper(i)
+make_instr_helper(r)
+make_instr_helper(rm)
+
+#include "cpu/exec/template-end.h"

c4bc6bbc2800df4611f4a60e66ec3c73711e1c2d
nbl97 <1035833260@qq.com>
2017-04-01 17:53:56 +0800

    修改了reg.h
    修改了cpu结构体

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -28,7 +28,18 @@ typedef struct {
 			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 		};
 	};
-
+	union{
+		struct {
+			uint32_t CF:1;
+			uint32_t PF:1;
+			uint32_t ZF:1;
+			uint32_t SF:1;
+			uint32_t IF:1;
+			uint32_t DF:1;
+			uint32_t OF:1;
+		};
+		uint32_t eflags;
+	}EFLAGS;
 	swaddr_t eip;
 
 } CPU_state;

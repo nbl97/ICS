422a7e853d24ba6f0cfd70c390c914e4f3f8cb6c
tracer-ics2015 <tracer@njuics.org>
2017-04-17 22:10:09 +0800

    >  compile NEMU 161530217 nibolin Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  22:10:09 up  2:32,  1 user,  load average: 0.10, 0.03, 0.01 2a691a45add9c8dd451bcfd24788a5456461ab8b

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -2,17 +2,20 @@
 
 #define instr push
 
-static void do_execute () {
-	if (DATA_BYTE == 1)op_src->val = (int8_t)op_src->val;
-	reg_l (R_ESP) -= 4;
-	swaddr_write (reg_l (R_ESP) , 4 , op_src->val);
+static void do_execute() {
+	if(DATA_BYTE == 2) {
+		cpu.esp -= 2;
+		swaddr_write(cpu.esp, 2, op_src->val);
+	}
+	else {
+		cpu.esp -= 4;
+		swaddr_write(cpu.esp, 4, op_src->val);
+	}
 	print_asm_template1();
 }
 
-make_instr_helper(i)
-#if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(r)
 make_instr_helper(rm)
-#endif
+make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"

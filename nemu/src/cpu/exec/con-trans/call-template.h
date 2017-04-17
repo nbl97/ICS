10f03931d09ec34faedcfcd9641970e711c33fb3
tracer-ics2015 <tracer@njuics.org>
2017-04-17 22:11:30 +0800

    >  compile NEMU 161530217 nibolin Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  22:11:30 up  2:33,  1 user,  load average: 0.10, 0.04, 0.01 2017722ed8f4b6ee95e83f10cb103d185d3c60cb

--- nemu/src/cpu/exec/con-trans/call-template.h
+++ nemu/src/cpu/exec/con-trans/call-template.h
@@ -2,7 +2,20 @@
 
 #define instr call
 
-static void do_execute() {}
+static void do_execute() {
+	if(op_src->type == OP_TYPE_IMM) {
+		cpu.esp -= DATA_BYTE;
+		if(DATA_BYTE == 2) {
+			MEM_W(cpu.esp, cpu.ip + DATA_BYTE + 1);
+			cpu.eip = (cpu.eip + op_src->val) & 0x0000ffff;
+		}
+		else if(DATA_BYTE == 4) {
+			MEM_W(cpu.esp, cpu.eip + DATA_BYTE + 1);
+			cpu.eip += op_src->val;
+		}
+	}
+	print_asm_template1();
+}
 
 make_instr_helper(i)
 
